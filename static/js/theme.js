e03143c31581d5b98087e22cc4242fb50d2246c9
UnCool <veevbajaj@gmail.com>
2024-04-28 20:53:23 +0530

    always dark

---
+++ static/js/theme.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // if (!localStorage.getItem('visited')) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('visited', 'true');
+    // }
+  });
+  
